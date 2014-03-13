6277a8a53e5ac0860db3ab7a0b069724fb594402
karen chan <karen@karen-chan.com>
2014-03-13 13:47:00 +0100

    Release version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='openstax-accounts',
-        version='0.2',
+        version='0.3',
         description='An example pyramid app that connects to openstax/accounts',
         long_description=open('README.rst').read(),
         author='Karen Chan',

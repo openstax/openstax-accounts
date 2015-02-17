e4448567071c61926f953e8a11d1b71ff0198ac5
Michael Mulich <michael.mulich@gmail.com>
2015-02-17 13:35:56 -0500

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='openstax-accounts',
-        version='0.13.0',
+        version='0.14.0',
         description='An example pyramid app that connects to openstax/accounts',
         long_description=open('README.rst').read(),
         author='Connexions team',
